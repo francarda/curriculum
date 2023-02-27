d45296d578b5b63e0c1658fdca40f08e4ddfd0ac
francarda <113646579+francarda@users.noreply.github.com>
2023-02-27 00:14:22 -0300

    antes de subirlo
    hover, fuentes, estilos y demas detalles

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ const array=["recursos/carpinteria.jpg",
 "recursos/parque1.jpg"
 ];
     let contador=0;
+    const imagen = document.getElementById("imagen-experiencia");
+   /* function frames(){
+        let animacion= document.getElementById("imagen-experiencia").animate([
+            0% {'opacity': 0},
+           25%{ 'opacity':0.25},
+           50% {'opacity' :0.58},
+           100%{'opacity':1},
+        ],{
+            easing: "linear",
+            iterations:1,
+            duration: 2000,
+        }); return animacion.finished;
+    }*/
 function carrusel(){
     
     
@@ -22,7 +35,8 @@ function carrusel(){
     
     if (contador<7){
        contador ++;
-        imagen.style.backgroundImage="url('"+array[contador]+"')";
+    imagen.src=array[contador];
+    /*window.setInterval(frames)*/
     console.log("hecho"+ contador);
     }
     else {contador=0;
